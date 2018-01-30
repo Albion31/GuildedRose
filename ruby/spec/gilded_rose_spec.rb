28e0623827c96a7af13969513da15d6b2afeb6a8
Lan Pham <lanpham312@gmail.com>
2018-01-30 13:01:20 +0000

    update sulfuras quality and add a 2nd test for over 50 quality

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -38,18 +38,25 @@ describe GildedRose do
       expect(items[0].quality).to eq 13
     end
 
-    it "The Quality of an item is never more than 50" do
+    it "The Quality of an item is never more than 50 - 1st test" do
       items = [Item.new("Aged Brie", 10, 50)]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq 9
       expect(items[0].quality).to eq 50
     end
 
+    it "The Quality of an item is never more than 50 - 2nd test" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 3, 48)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 2
+      expect(items[0].quality).to eq 50
+    end
+
     it "'Sulfuras', being a legendary item, never has to be sold or decreases in Quality" do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 20)]
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 80)]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq 10
-      expect(items[0].quality).to eq 20
+      expect(items[0].quality).to eq 80
     end
 
     it "'Backstage passes' increases in Quality by 2 when there are 10 days or less" do
@@ -73,5 +80,4 @@ describe GildedRose do
       expect(items[0].quality).to eq 0
     end
   end
-
 end
