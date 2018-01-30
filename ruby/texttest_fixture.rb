a0fc7302e840b74047c2d2e5bc9321ce9d915e82
Lan Pham <lanpham312@gmail.com>
2018-01-30 14:43:20 +0000

    setting up texttest and refactoring to constants

--- ruby/texttest_fixture.rb
+++ ruby/texttest_fixture.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby -w
 
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require File.join(File.dirname(__FILE__), './lib/gilded_rose')
 
 puts "OMGHAI!"
 items = [
@@ -16,10 +16,10 @@ items = [
   Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
 ]
 
-days = 2
-if ARGV.size > 0
-  days = ARGV[0].to_i + 1
-end
+days = 5
+# if ARGV.size > 0
+#   days = ARGV[0].to_i + 1
+# end
 
 gilded_rose = GildedRose.new items
 (0...days).each do |day|
