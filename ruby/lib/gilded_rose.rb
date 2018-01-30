2b940a5c33caf6b371cbf81d5eaacc6e8ef8895f
Lan Pham <lanpham312@gmail.com>
2018-01-30 15:58:46 +0000

    refactoring sell in, drop backstage pass quality and increase brie quality methods

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -12,11 +12,11 @@ class GildedRose
     @items.each do |item|
       if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS
         if item.quality > 0
-          if item.name != SULFURAS
-            item.quality = item.quality - 1
-          end
+          update_sell_in(item)
         end
+
       else
+
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == BACKSTAGE_PASS
@@ -33,9 +33,11 @@ class GildedRose
           end
         end
       end
+
       if item.name != SULFURAS
         item.sell_in = item.sell_in - 1
       end
+
       if item.sell_in < 0
         if item.name != AGED_BRIE
           if item.name != BACKSTAGE_PASS
@@ -45,15 +47,29 @@ class GildedRose
               end
             end
           else
-            item.quality = item.quality - item.quality
+            drop_backstage_pass_quality_after_concert(item)
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          increase_brie_quality_after_SOD(item)
         end
       end
     end
+  end
+end
+
+def update_sell_in(item)
+  if item.name != "Sulfuras, Hand of Ragnaros"
+    item.quality = item.quality - 1
+  end
+end
+
+def drop_backstage_pass_quality_after_concert(item)
+  item.quality = item.quality - item.quality
+end
+
+def increase_brie_quality_after_SOD(item)
+  if item.quality < 50
+    item.quality = item.quality + 1
   end
 end
 
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,5 +1,6 @@
-# require File.join(File.dirname(__FILE__), './lib/gilded_rose')
 require "spec_helper"
+require File.join(File.dirname(__FILE__), '../lib/gilded_rose')
+
 
 describe GildedRose do
 
@@ -31,11 +32,25 @@ describe GildedRose do
       expect(items[0].quality).to eq 0
     end
 
+    it "The Quality of an item is never negative - 2nd test" do
+      items = [Item.new("milk", 2, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 1
+      expect(items[0].quality).to eq 0
+    end
+
     it "'Aged Brie' actually increases in Quality the older it gets" do
       items = [Item.new("Aged Brie", 2, 12)]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq 1
       expect(items[0].quality).to eq 13
+    end
+
+    it "'Aged Brie' increases Quality by 2 after the sell out date" do
+      items = [Item.new("Aged Brie", 0, 12)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq (-1)
+      expect(items[0].quality).to eq 14
     end
 
     it "The Quality of an item is never more than 50 - 1st test" do
