80fa9d5152073eb8f4da5fa5ae77a6c38bfdf529
Lan Pham <lanpham312@gmail.com>
2018-01-31 11:17:35 +0000

    refactoring if statement for positive sell in date for brie and backstage pass

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -9,12 +9,13 @@ class GildedRose
   end
 
   def update_quality()
+
     @items.each do |item|
       if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS
         reduce_quality(item)
       else
         if item.quality < 50
-          item.quality = item.quality + 1
+          item.quality += 1
           if item.name == BACKSTAGE_PASS
             if item.sell_in < 11
               increase_quality(item)
@@ -26,23 +27,21 @@ class GildedRose
         end
       end
 
-      if item.name != SULFURAS
-        item.sell_in = item.sell_in - 1
-      end
+      update_quantity_if_not_sulfuras(item)
 
       if item.sell_in < 0
-        if item.name != AGED_BRIE
-          if item.name != BACKSTAGE_PASS
-            reduce_quality(item)
-          else
-            drop_backstage_pass_quality_after_concert(item)
-          end
-        else
+        if item.name == AGED_BRIE
           increase_quality(item)
+        elsif item.name == BACKSTAGE_PASS
+          drop_backstage_pass_quality_after_concert(item)
+        else
+          reduce_quality(item)
         end
       end
     end
+
   end
+
 end
 
 def reduce_quality(item)
@@ -57,9 +56,16 @@ def update_quality_if_not_sulfuras(item)
   end
 end
 
+def update_quantity_if_not_sulfuras(item)
+  if item.name != "Sulfuras, Hand of Ragnaros"
+    item.sell_in -= 1
+  end
+end
+
 def drop_backstage_pass_quality_after_concert(item)
   item.quality -= item.quality
 end
+
 
 def increase_quality(item)
   if item.quality < 50
